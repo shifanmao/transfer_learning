6b38db4ab0063a4dea94b682c00f4a7465aedaa6
IFDYS <wayne.weiqiang@gmail.com>
2017-02-23 18:08:21 -0800

    add proccessed _light.csv

--- tranfer_learning/data_processing.py
+++ tranfer_learning/data_processing.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from bs4 import BeautifulSoup
+import nltk
+nltk.data.path.append('../data/nltk_data/')
 from nltk.corpus import stopwords
 import re
 import string
